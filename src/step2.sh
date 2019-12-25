03acdb424b96efce9c9923360da18b4a6e05e3c2
汪梦竹 <642053372@qq.com>
2019-12-26 00:18:35 +0800

    Edit by browser

--- src/step2.sh
+++ src/step2.sh
@@ -3,8 +3,10 @@ rm ./task2/* > /dev/null 2<&1
 rm ./src/trans.o  > /dev/null 2<&1
 rm ./src/test-trans  > /dev/null 2<&1
 cd ./src  > /dev/null 2<&1
+dir
 gcc -g -Wall -Werror -std=c99 -m64 -O0 -c trans.c >/dev/null 2>&1
 gcc -g -Wall -Werror -std=c99 -m64 -o test-trans test-trans.c cachelab.c trans.o  >/dev/null 2>&1
+gcc -g -Wall -Werror -std=c99 -m64 -O0 -o tracegen tracegen.c trans.o cachelab.c >/dev/null 2>&1
 python driver_tans.py > info 2>&1
 cat info | grep "程序没有改进" >result 2>&1
 if [ $? -eq 0 ]; then
@@ -12,5 +14,4 @@ if [ $? -eq 0 ]; then
 else
     cat info
 fi
-cd ..  >/dev/null 2>&1
-cat ./src/info > ./task2/step2.txt  >/dev/null 2>&1
+cat info > ../task2/step2.txt   2>&1
