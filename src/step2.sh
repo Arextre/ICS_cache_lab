74e49984fe2ee53ecf197beb897e010a50b46909
汪梦竹 <642053372@qq.com>
2019-12-25 23:19:39 +0800

    step2

---
+++ src/step2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm ./task2/* > /dev/null 2<&1
+rm ./src/trans.o  > /dev/null 2<&1
+rm ./src/test-trans  > /dev/null 2<&1
+cd ./src  > /dev/null 2<&1
+gcc -g -Wall -Werror -std=c99 -m64 -O0 -c trans.c >/dev/null 2>&1
+gcc -g -Wall -Werror -std=c99 -m64 -o test-trans test-trans.c cachelab.c trans.o  >/dev/null 2>&1
+python driver_tans.py > info 2>&1
+cat info | grep "程序没有改进" >result 2>&1
+if [ $? -eq 0 ]; then
+    echo "程序通过"
+else
+    cat info
+fi
+cd ..  >/dev/null 2>&1
+cat ./src/info > ./task2/step2.txt  >/dev/null 2>&1
