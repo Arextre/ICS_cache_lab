935fddd85e52daa6f78d18ad834d363cdccb127d
汪梦竹 <642053372@qq.com>
2019-12-25 23:24:42 +0800

    Edit by browser

--- src/step1.sh
+++ src/step1.sh
@@ -4,7 +4,7 @@ rm ./src/csim  > /dev/null 2<&1
 cd ./src  > /dev/null 2<&1
 gcc -g -Wall -m64 -o csim csim.c cachelab.c -lm >/dev/null 2>&1
 python driver.py > info 2>&1
-cat info | grep "Error running test simulator" >result 2>&1
+cat info | grep "程序没有改进" >result 2>&1
 if [ $? -eq 0 ]; then
     echo "程序通过"
 else
