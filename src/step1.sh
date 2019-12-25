d622c8dcfc15a5af81fabebb60a4cfd238c20301
汪梦竹 <642053372@qq.com>
2019-12-25 23:59:58 +0800

    Edit by browser

--- src/step1.sh
+++ src/step1.sh
@@ -5,7 +5,6 @@ cd ./src  > /dev/null 2<&1
 gcc -g -Wall -Werror -std=c99 -m64 -o csim csim.c cachelab.c -lm  >/dev/null 2>&1
 gcc -g -Wall -Werror -std=c99 -m64 -o csim-ref csim-ref.c cachelab.c -lm >/dev/null 2>&1
 gcc -g -Wall -Werror -std=c99 -m64 -o test-csim test-csim.c >/dev/null 2>&1
-dir
 python driver.py > info 2>&1
 cat info | grep "程序没有改进" >result 2>&1
 if [ $? -eq 0 ]; then
