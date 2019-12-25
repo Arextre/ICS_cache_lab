0ae05da4050dbd13093f82ea39fc81326be451d0
15223006811 <642053372@qq.com>
2019-12-25 22:44:29 +0800

    c1

---
+++ src/trans.c
@@ -0,0 +1,72 @@
+/* 
+ * trans.c - 矩阵转置B=A^T
+ *每个转置函数都必须具有以下形式的原型：
+ *void trans（int M，int N，int a[N][M]，int B[M][N]）；
+ *通过计算，块大小为32字节的1KB直接映射缓存上的未命中数来计算转置函数。
+ */ 
+#include <stdio.h>
+#include "cachelab.h"
+
+int is_transpose(int M, int N, int A[N][M], int B[M][N]);
+
+/* 
+ *transpose_submit -  这是一个解决方案转置函数，你将在实验部分评分。
+ *不要更改描述字符串“Transpose submission”，因为驱动程序会搜索该字符串以标识要分级的Transpose函数。
+ */
+char transpose_submit_desc[] = "Transpose submission";
+void transpose_submit(int M, int N, int A[N][M], int B[M][N])
+{
+}
+
+/* 
+ * 您可以在下面定义其他转置函数。我们在下面定义了一个简单的方法来帮助您开始。
+ */ 
+
+/* 
+ * 简单的基线转置功能，未针对缓存进行优化。
+ */
+char trans_desc[] = "Simple row-wise scan transpose";
+void trans(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, tmp;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            tmp = A[i][j];
+            B[j][i] = tmp;
+        }
+    }    
+
+}
+
+/*
+ * registerFunctions-此函数向驱动程序注册转置函数。
+ *在运行时，驱动程序将评估每个注册的函数并总结它们的性能。这是一种试验不同转置策略的简便方法。
+ */
+void registerFunctions()
+{
+    /* 注册解决方案函数  */
+    registerTransFunction(transpose_submit, transpose_submit_desc); 
+
+    /* 注册任何附加转置函数 */
+    registerTransFunction(trans, trans_desc); 
+
+}
+
+/* 
+ * is_transpose - 函数检查B是否是A的转置。在从转置函数返回之前，可以通过调用它来检查转置的正确性。
+ */
+int is_transpose(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; ++j) {
+            if (A[i][j] != B[j][i]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
